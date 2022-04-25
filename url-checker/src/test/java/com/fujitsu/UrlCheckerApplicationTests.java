a05f17ac90f69358b452d69f65616a435223c9f6
Hubert Berja <bert.stark3231@gmail.com>
2022-04-25 23:29:59 +0800

    initial commit

---
+++ url-checker/src/test/java/com/fujitsu/UrlCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujitsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
